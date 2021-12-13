591afc542038b04086077b50162088ad41de2d0d
Karlven <karlven@outlook.com>
2021-12-13 20:38:19 +0800

    pattern match

--- src/lib.rs
+++ src/lib.rs
@@ -66,3 +66,167 @@ pub mod circle {
         }
     }
 }
+
+pub mod pattern_match {
+    use crate::utils::put_blank_line;
+
+    pub fn practice() {
+        enum Direction {
+            East,
+            West,
+            North,
+            South,
+        }
+        let dire = Direction::South;
+        match dire {
+            Direction::East => println!("East"),
+            Direction::North | Direction::South => {
+                println!("South or North");
+            },
+            _ => println!("West"),
+        };
+
+        let d_west = Direction::West;
+        let d_str = match d_west {
+            Direction::East => "East",
+            Direction::North | Direction::South => {
+                panic!("South or North");
+            },
+            _ => "West",
+        };
+        println!("{}", d_str);
+        put_blank_line();
+
+        destructure();
+        pattern();
+        range();
+        reference();
+        binding();
+    }
+
+    fn binding() {
+        let x = 1u32;
+        match x {
+            e @ 1..=5 | e @ 10..=15 => println!("get {}", e),
+            _ => {}
+        }
+
+        put_blank_line();
+    }
+
+    fn reference() {
+        let mut x = 5;
+        match x {
+            ref mut mr => println!("mut ref :{}", mr),
+        }
+        let ref mut mrx = x;
+        put_blank_line();
+    }
+
+    fn range() {
+        let x = 1;
+        match x {
+            1 ..= 10 => println!("1 to 10"),
+            _ => println!("other"),
+        }
+
+        let c = 'w';
+
+        match c {
+            'a' ..= 'z' => println!("lowercase"),
+            'A' ..= 'Z' => println!("uppercase"),
+            _ => println!("other"),
+        }
+
+        let x = 1;
+        match x {
+            1 | 2 => println!("1 or 2"),
+            _ => println!("other"),
+        }
+    }
+
+    fn destructure() {
+        enum Action {
+            Say(String),
+            MoveTo(i32, i32),
+            ChangeColorRGB(u16, u16, u16),
+        }
+
+        let action = Action::Say("Hello Boy".to_string());
+        // let action = Action::ChangeColorRGB(255, 255, 255);
+        match action {
+            Action::Say(s) => {
+                println!("{}", s);
+            },
+            Action::MoveTo(x, y) => {
+                println!("point from (0, 0) move to ({}, {})", x, y);
+            },
+            Action::ChangeColorRGB(r,g,_) => {
+                println!("change color into '(r:{}, g:{}, b:0)', 'b' has been ignored",r, g);
+            }
+        }
+
+        struct Point {
+            x: i64,
+            y: i64,
+        }
+        let point = Point{
+            x: 0,
+            y: 0,
+        };
+        match point {
+            Point{
+                x, y
+            } => println!("({}, {})", x, y),
+        }
+        //or
+        match point {
+            Point {
+                x: x1,
+                y: y1,
+            } => println!("({}, {})", x1, y1),
+        }
+
+        let point = Point{
+            x: 0,
+            y: 0,
+        };
+        match point {
+            Point{y,..} => println!("y is {}", y),
+        }
+
+
+        let tuple: (u32, String) = (5, String::from("five"));
+
+        let (x, s) = tuple;
+        //String没有实现Copy， 所以tuple被整个move掉了，所以下面的代码编译错误
+        //println!("Tuple is : {:?}", tuple);
+
+        let tuple = (5, String::from("five"));
+        //String被_忽略了， i32实现了Copy，所以tuple没有被move
+        let (x, _) = tuple;
+        println!("{:?}", tuple);
+
+        put_blank_line();
+    }
+
+    fn pattern() {
+        let x = 1;
+        let c = 'c';
+        match c {
+            x => println!("[x in match]: {}, [c]: {}", x, c),
+        }
+        println!("outer [x]:{}", x);
+        put_blank_line();
+
+        let x = 4;
+        let y = false;
+        match x {
+            4|5 if y => println!("yes"),
+            _ => println!("no"),
+        }
+        put_blank_line();
+    }
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use rust_primer::vector;
 use rust_primer::circle;
 use rust_primer::my_string;
 use rust_primer::ownership;
+use rust_primer::pattern_match;
 
 fn main() {
     println!("Hello, world!");
@@ -13,4 +14,5 @@ fn main() {
     multi_type::enumerate::practice();
     my_string::practice();
     ownership::practice();
+    pattern_match::practice();
 }
